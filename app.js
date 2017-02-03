4afc6fedf8639f103649b1da0c31dc716c0b24a2
unknown <clara.mostacchi@gmail.com>
2017-02-03 13:11:35 +0100

    Sart TD3

--- app.js
+++ app.js
@@ -25,3 +25,31 @@ app.get( '/', function ( req, res ) {
 app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
+
+request( 'https://www.leboncoin.fr/ventes_immobilieres/1066912654.htm?ca=12_s', function ( error, response, html ) {
+    if ( !error && response.statusCode == 200 ) {
+        var $ = cheerio.load( html );
+        var price = parseInt( $( 'h2.item_price span.value' ).get()[0].children[0].data.replace( /\s/g, '' ), 10 );
+        console.log( price );
+
+        var ville = $( 'h2 .value ' ).get()[1].children[0].data.split( " " );
+        console.log( ville[0] );
+
+        var cp = $( 'h2 .value ' ).get()[1].children[0].data.replace( /\n/g, '' ).split( " " );
+        console.log( cp[1] );
+
+        var typedebien = $( 'h2 .value ' ).get()[2].children[0].data;
+        console.log( typedebien );
+
+        var nbPiece = $( 'h2 .value ' ).get()[3].children[0].data;
+        console.log( nbPiece );
+
+        var surface = parseInt( $( 'h2 .value ' ).get()[4].children[0].data.replace( /\s/g, '' ), 10 );
+        console.log( surface );
+
+
+
+
+    };
+
+});
